68a221e191559e242d9aaa379dc70057ffd65300
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-06-08 21:32:35 -0400

    Bookmark model validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,12 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :user
+
+  # ensures that a user_id is present
+  validates :user_id, presence: true
+
+  # ensure that title is present and at least 10 chars long
+  validates :title, length: { minimum: 10 }, presence: true
+
+  # ensure url is present and in proper format
+  validates :url, format: { with: Regexp.new(URI::regexp(%w(http https))) }, presence: true
 end
